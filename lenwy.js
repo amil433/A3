0a8a4fe9b355564d6b587a20f71219b6c207de8e
Abdul Malik <141676888+JustNotSec@users.noreply.github.com>
2024-02-26 00:43:37 +0700

    Update lenwy.js

--- lenwy.js
+++ lenwy.js
@@ -9777,7 +9777,6 @@ let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 fs.unwatchFile(file)
 console.log(chalk.redBright(`Update ${__filename}`))
-reply(`Update ${__filename}`)
 lenwy.sendMessage('6288294744259' + "@s.whatsapp.net", { text: `Update ${__filename} ` + util.format(e), 
 contextInfo:{
 forwardingScore: 9999999, 
